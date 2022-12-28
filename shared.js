10c9fbaf5b338e3f0dc327ae6894e553ea151f23
Ayan Liaqat <ayanliaqat48@gmail.com>
2022-12-28 19:36:27 +0600

    Added modal and sidebar navigation menu

---
+++ shared.js
@@ -0,0 +1,33 @@
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelector(".modal");
+let modalNoButton = document.querySelector(".modal__action--negative");
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
+
+for(i = 0; i <= selectPlanButtons.length; i++) {
+    if(selectPlanButtons[i]) {
+    selectPlanButtons[i].addEventListener("click", () => {
+       backdrop.style.display =  "block";
+        modal.style.display = "block";
+    })
+}
+}
+
+const closeModal = () => {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+}
+
+backdrop.addEventListener("click", () => {
+    mobileNav.style.display = "none";
+    closeModal();
+})
+
+modalNoButton.addEventListener("click", closeModal);
+
+toggleButton.addEventListener("click", () => {
+    mobileNav.style.display = "block";
+    backdrop.style.display = "block"; 
+})
+
